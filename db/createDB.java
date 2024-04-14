185d2267a4480aac82b80444e401c1eaa20f188f
Danny <daniel.silva010@mymdc.net>
2024-04-14 17:45:18 -0400

    uploaded changes to work from my other pc

--- db/createDB.java
+++ db/createDB.java
@@ -1,1 +1,16 @@
-//implement
+public class createDB {
+    public static void main(String[] args) {
+        String url = System.getenv("URL");
+        String dbName = "MDC";
+        String user = System.getenv("USER");
+        String password = System.getenv("PASSWORD");
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            String sql = "CREATE DATABASE " + dbName;
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
--- db/createTables.java
+++ db/createTables.java
@@ -1,1 +1,14 @@
-//implement
+public class createTables {
+    public static void main(String[] args) {
+        String url = System.getenv("URL");
+        String dbName = "MDC";
+        String user = System.getenv("USER");
+        String password = System.getenv("PASSWORD");
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            // String sql = "CREATE TABLE IF NOT EXISTS Students
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
