1100168d1faad0c7bd81a6ee5f025052822ce417
Danny <daniel.silva010@mymdc.net>
2024-04-14 11:40:04 -0400

    closes #2

--- src/Controllers/ModifyGradesController.java
+++ src/Controllers/ModifyGradesController.java
@@ -1,7 +1,11 @@
 package Controllers;
 
+import DAOs.CourseRosterDAO;
+import Models.CourseRoster;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.TextField;
 
 public class ModifyGradesController {
@@ -17,12 +21,47 @@ public class ModifyGradesController {
 
     @FXML
     void modify(ActionEvent event) {
-
+        if (!tfStudentID.getText().equals("Enter Student ID Here") && !tfStudentID.getText().equals("")
+                && !tfCRN.getText().equals("Enter CRN Here") && !tfCRN.getText().equals("")) {
+            if (CourseRosterDAO.updateGradeForStudent(tfStudentID.getText(), Long.parseLong(tfCRN.getText()),
+                    tfGrade.getText())) {
+                tfGrade.setDisable(true);
+                tfCRN.setDisable(true);
+                tfStudentID.setDisable(true);
+                Alert alert = new Alert(AlertType.INFORMATION);
+                alert.setTitle("Success");
+                alert.setHeaderText("Grade Updated");
+                alert.setContentText(
+                        "Grade for " + tfStudentID.getText() + " has been updated to " + tfGrade.getText());
+                alert.showAndWait();
+            }
+        } else {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Invalid CRN or Student ID");
+            alert.setContentText("Please enter a valid CRN and Student ID.");
+            alert.showAndWait();
+        }
     }
 
     @FXML
     void search(ActionEvent event) {
-
+        if (!tfStudentID.getText().equals("Enter Student ID Here") && !tfStudentID.getText().equals("")
+                && !tfCRN.getText().equals("Enter CRN Here") && !tfCRN.getText().equals("")) {
+            CourseRoster oneRoster = CourseRosterDAO.getOneRosterForStudent(tfStudentID.getText(),
+                    Long.parseLong(tfCRN.getText()));
+            if (oneRoster != null) {
+                tfGrade.setText(oneRoster.getGrade());
+                tfCRN.setDisable(true);
+                tfStudentID.setDisable(true);
+            }
+        } else {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Invalid CRN or Student ID");
+            alert.setContentText("Please enter a valid CRN and Student ID.");
+            alert.showAndWait();
+        }
     }
 
 }
--- src/DAOs/CourseRosterDAO.java
+++ src/DAOs/CourseRosterDAO.java
@@ -1,5 +1,7 @@
 package DAOs;
 
+import static Utils.dataUtilities.convertGradeToDouble;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -7,9 +9,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import Models.CourseRoster;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
-import static Utils.dataUtilities.convertGradeToDouble;
 
 public class CourseRosterDAO {
 
@@ -35,6 +37,53 @@ public class CourseRosterDAO {
             return false;
         }
 
+    }
+
+    public static CourseRoster getOneRosterForStudent(String studentID, long CRN) {
+        String user = System.getenv("USER");
+        String password = System.getenv("PASSWORD");
+        String url = System.getenv("URL");
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            String sql = "SELECT * FROM CourseRoster WHERE studentID = ? AND CRN = ?";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, studentID);
+            pstmt.setLong(2, CRN);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return new CourseRoster(CRN, studentID, rs.getString("grade"));
+            }
+        } catch (SQLException e) {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Database Error");
+            alert.setContentText(e.getMessage());
+            alert.showAndWait();
+        }
+        return null;
+    }
+
+    public static boolean updateGradeForStudent(String studentID, long CRN, String grade) {
+        String user = System.getenv("USER");
+        String password = System.getenv("PASSWORD");
+        String url = System.getenv("URL");
+
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            String sql = "UPDATE CourseRoster SET grade = ? WHERE studentID = ? AND CRN = ?";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, grade);
+            pstmt.setString(2, studentID);
+            pstmt.setLong(3, CRN);
+            pstmt.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Database Error");
+            alert.setContentText(e.getMessage());
+            alert.showAndWait();
+            return false;
+
+        }
     }
 
     public static ArrayList<Double> getGradesForCourse(long CRN) {
